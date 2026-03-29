3f02127f03a9beff5b462260efdbe540835ddfec
Aparna Kalla <56730662+aparnakalla@users.noreply.github.com>
2026-03-29 00:06:21 -0400

    Delete patches.py

--- patches.py
+++
@@ -1,9 +0,0 @@
-import sys
-import types
-
-if 'pkg_resources' not in sys.modules:
-    pkg_resources = types.ModuleType('pkg_resources')
-    pkg_resources.get_distribution = lambda name: type('D', (), {'version': '0.0.0'})()
-    pkg_resources.DistributionNotFound = Exception
-    pkg_resources.RequirementParseError = Exception
-    sys.modules['pkg_resources'] = pkg_resources
